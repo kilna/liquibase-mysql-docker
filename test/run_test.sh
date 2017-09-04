98cfdcf577b68e2f183920812ed7f62fe5074dbc
Kilna <kilna@kilna.com>
2017-09-04 16:36:18 -0500

    Initial development

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-set -e -o pipefail
+#set -e -o pipefail
+
+until nc -z -v -w1 testdb 3306; do
+  echo "Waiting for database connection..."
+  sleep 3
+done
 
 echo "Applying changelog"
 liquibase updateTestingRollback
